70a2a74827e06865212ce155707f342976997da0
Wojciech Szlosek <wojciech185sz@gmail.com>
2021-12-04 12:44:51 +0100

    add Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,10 +1,30 @@
+import kotlin.collections.*
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var result = 0
+
+        for (i in 1 until input.size) {
+            if (input[i - 1] < input[i]) {
+                result += 1
+            }
+        }
+
+        return result
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var result = 0
+
+        for (i in input.indices) {
+            if (i > 2) {
+                if (input[i - 3].toInt() < input[i].toInt()) {
+                    result += 1
+                }
+            }
+        }
+
+        return result
     }
 
     val input = readInput("Day01")
