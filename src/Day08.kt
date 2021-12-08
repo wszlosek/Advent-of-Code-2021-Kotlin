1b0be98050beca0a0a5fe550bf6a526d407378c8
Wojciech Szlosek <wojciech185sz@gmail.com>
2021-12-08 15:26:02 +0100

    update part 1

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,15 +1,17 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        var result = 0
+        var x = 0
         for (i in input) {
-            val outputValues = i.split(" | ").last().split(" ")
-            for (p in outputValues) {
-                if (p.length == 2 || p.length == 3 || p.length == 4 || p.length == 7) {
-                    result += 1
+            val outputValues = i
+                .split(" | ")
+                .last()
+                .split(" ")
+                .filter {
+                    it.length == 2 || it.length == 3 || it.length == 4 || it.length == 7
                 }
-            }
+            x += outputValues.size
         }
-        return result
+        return x
     }
 
     val input = readInput("Day08")
